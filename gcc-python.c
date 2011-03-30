30ec4693f1ac56c683bfd8767b8bd941e48eae6f
David Malcolm <dmalcolm@redhat.com>
2011-03-30 18:21:36 -0400

    Wire up a set of generic callbacks, whilst exploring the interface

--- gcc-python.c
+++ gcc-python.c
@@ -4,6 +4,27 @@ int plugin_is_GPL_compatible;
 
 #include "plugin-version.h"
 
+static const char* event_name[] = {
+#define DEFEVENT(NAME) \
+  #NAME, 
+# include "plugin.def"
+# undef DEFEVENT
+};
+
+static void
+my_callback(enum plugin_event event, void *gcc_data, void *user_data)
+{
+  printf("%s:%i:my_callback(%s, %p, %p)\n", __FILE__, __LINE__, event_name[event], gcc_data, user_data);
+}
+
+#define DEFEVENT(NAME) \
+static void my_callback_for_##NAME(void *gcc_data, void *user_data) \
+{ \
+     my_callback(NAME, gcc_data, user_data); \
+}
+# include "plugin.def"
+# undef DEFEVENT
+
 int
 plugin_init (struct plugin_name_args *plugin_info,
              struct plugin_gcc_version *version)
@@ -12,8 +33,23 @@ plugin_init (struct plugin_name_args *plugin_info,
           return 1;
     }
 
-    printf("%s:%i:plugin_init", __FILE__, __LINE__);
+    printf("%s:%i:plugin_init\n", __FILE__, __LINE__);
+
+#define DEFEVENT(NAME) \
+    if (NAME != PLUGIN_PASS_MANAGER_SETUP &&         \
+        NAME != PLUGIN_INFO &&                       \
+	NAME != PLUGIN_REGISTER_GGC_ROOTS &&         \
+	NAME != PLUGIN_REGISTER_GGC_CACHES) {        \
+    register_callback(plugin_info->base_name, NAME,  \
+		      my_callback_for_##NAME, NULL); \
+    }
+# include "plugin.def"
+# undef DEFEVENT
+
   
-    //register_callback (plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+    //    register_callback (plugin_info->base_name, PLUGIN_ALL_PASSES_START,
+    //		       my_callback, NULL); 
+    //    register_callback (plugin_info->base_name, PLUGIN_ALL_PASSES_END,
+    //		       my_callback, NULL); 
     return 0;
 }
