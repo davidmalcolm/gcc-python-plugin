d3c6197525e9e43d579b8248f72190031a8b0406
David Malcolm <dmalcolm@redhat.com>
2011-03-30 18:48:13 -0400

    Add notes on the callback data types for the various events

--- gcc-python.c
+++ gcc-python.c
@@ -11,6 +11,75 @@ static const char* event_name[] = {
 # undef DEFEVENT
 };
 
+/*
+  Notes on the passes
+
+  As of 2011-03-30, http://gcc.gnu.org/onlinedocs/gccint/Plugins.html doesn't
+  seem to document the type of the gcc_data passed to each callback.
+
+  For reference, with gcc-4.6.0-0.15.fc15.x86_64 the types seem to be as
+  follows:
+
+  PLUGIN_ATTRIBUTES:
+    gcc_data=0x0 
+    Called from: init_attributes () at ../../gcc/attribs.c:187
+
+  PLUGIN_PRAGMAS:
+    gcc_data=0x0 
+    Called from: c_common_init () at ../../gcc/c-family/c-opts.c:1052
+
+  PLUGIN_START_UNIT:
+    gcc_data=0x0
+    Called from: compile_file () at ../../gcc/toplev.c:573
+
+  PLUGIN_PRE_GENERICIZE
+    gcc_data is:  tree fndecl;
+    Called from: finish_function () at ../../gcc/c-decl.c:8323
+
+  PLUGIN_OVERRIDE_GATE
+    gcc_data:
+      &gate_status
+      bool gate_status;
+    Called from : execute_one_pass (pass=0x1011340) at ../../gcc/passes.c:1520
+
+  PLUGIN_PASS_EXECUTION
+    gcc_data: struct opt_pass *pass
+    Called from: execute_one_pass (pass=0x1011340) at ../../gcc/passes.c:1530
+
+  PLUGIN_ALL_IPA_PASSES_START
+    gcc_data=0x0
+    Called from: ipa_passes () at ../../gcc/cgraphunit.c:1779
+
+  PLUGIN_EARLY_GIMPLE_PASSES_START
+    gcc_data=0x0
+    Called from: execute_ipa_pass_list (pass=0x1011fa0) at ../../gcc/passes.c:1927
+
+  PLUGIN_EARLY_GIMPLE_PASSES_END
+    gcc_data=0x0
+    Called from: execute_ipa_pass_list (pass=0x1011fa0) at ../../gcc/passes.c:1930
+
+  PLUGIN_ALL_IPA_PASSES_END
+    gcc_data=0x0
+    Called from: ipa_passes () at ../../gcc/cgraphunit.c:1821
+
+  PLUGIN_ALL_PASSES_START
+    gcc_data=0x0
+    Called from: tree_rest_of_compilation (fndecl=0x7ffff16b1f00) at ../../gcc/tree-optimize.c:420
+
+  PLUGIN_ALL_PASSES_END
+    gcc_data=0x0
+    Called from: tree_rest_of_compilation (fndecl=0x7ffff16b1f00) at ../../gcc/tree-optimize.c:425
+
+  PLUGIN_FINISH_UNIT
+    gcc_data=0x0
+    Called from: compile_file () at ../../gcc/toplev.c:668
+
+  PLUGIN_FINISH
+    gcc_data=0x0
+    Called from: toplev_main (argc=17, argv=0x7fffffffdfc8) at ../../gcc/toplev.c:1970
+
+*/
+
 static void
 my_callback(enum plugin_event event, void *gcc_data, void *user_data)
 {
@@ -46,10 +115,5 @@ plugin_init (struct plugin_name_args *plugin_info,
 # include "plugin.def"
 # undef DEFEVENT
 
-  
-    //    register_callback (plugin_info->base_name, PLUGIN_ALL_PASSES_START,
-    //		       my_callback, NULL); 
-    //    register_callback (plugin_info->base_name, PLUGIN_ALL_PASSES_END,
-    //		       my_callback, NULL); 
     return 0;
 }
