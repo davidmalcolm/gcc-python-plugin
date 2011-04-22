5194ff33ab74176b986ae95985abcdd7e757177b
David Malcolm <dmalcolm@redhat.com>
2011-04-22 10:34:02 -0400

    Make the sample c code a little more interesting

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
 /*
   Trivial example code to be compiled, for testing purposes
  */
+
+#include <stdio.h>
+
+int
+helper_function(void)
+{
+    printf("I am a helper function\n");
+    return 42;
+}
+
 int
 main(int argc, char **argv)
 {
+    int i;
+
+    printf("argc: %i\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        printf("argv[%i]: %s\n", argv[i]);
+    }
+
+    helper_function();
+
     return 0;
 }
