4b9963bf57e7416cdbc46722a803078fceb05710
David Malcolm <dmalcolm@redhat.com>
2011-06-03 12:20:38 -0400

    Start adding a visualizer for trees (refactoring the visualizer for CFGs)

--- gccutils.py
+++ gccutils.py
@@ -24,11 +24,9 @@ def invoke_dot(dot):
     p.communicate()
     
 
-class CfgPrettyPrinter:
-    # Generate graphviz source for this gcc.Cfg instance, as a string
-    def __init__(self, cfg):
-        self.cfg = cfg
-
+class DotPrettyPrinter:
+    # Base class for various kinds of data visualizations that use graphviz
+    # (aka ".dot" source files)
     def to_html(self, text):
         html_escape_table = {
             "&": "&amp;",
@@ -43,13 +41,6 @@ class CfgPrettyPrinter:
           }
         return "".join(html_escape_table.get(c,c) for c in str(text))
 
-    def block_id(self, b):
-        if b is self.cfg.entry:
-            return 'entry'
-        if b is self.cfg.exit:
-            return 'exit'
-        return 'block%i' % id(b)
-
     def _dot_td(self, text, align="left", colspan=1, escape=1, bgcolor=None):
         if escape:
             text = self.to_html(text)
@@ -61,6 +52,19 @@ class CfgPrettyPrinter:
 
     def _dot_tr(self, td_text):
         return ('<tr>%s</tr>\n' % self._dot_td(td_text))
+
+
+class CfgPrettyPrinter(DotPrettyPrinter):
+    # Generate graphviz source for this gcc.Cfg instance, as a string
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def block_id(self, b):
+        if b is self.cfg.entry:
+            return 'entry'
+        if b is self.cfg.exit:
+            return 'exit'
+        return 'block%i' % id(b)
 
     def block_to_dot_label(self, bb):
         # FIXME: font setting appears to work on my machine, but I invented
@@ -152,7 +156,67 @@ class CfgPrettyPrinter:
         result += '}\n'
         return result
 
+class TreePrettyPrinter(DotPrettyPrinter):
+    # Generate a graphviz visualization of this gcc.Tree and the graphs of
+    # nodes it references, as a string
+    def __init__(self, root):
+        print 'root: %s' % root
+        assert isinstance(root, gcc.Tree)
+        self.root = root
+
+    def attr_to_str(self, name, value):
+        if name == 'addr':
+            return hex(value)
+        if isinstance(value, str):
+            return repr(value)
+        return str(value)
+
+    def label_for_tree(self, obj):
+        result = '<table cellborder="0" border="0" cellspacing="0">\n'
+        result += ('<tr> %s </tr>\n'
+                   % (self._dot_td(obj, colspan=2)))
+        #self._dot_td('')))
+        for name, value in self.iter_attrs(obj):
+            result += ('<tr> %s %s </tr>\n'
+                       % (self._dot_td(name),
+                          self._dot_td(self.attr_to_str(name, value))))
+        result += '</table>\n'
+        return result
+
+    def iter_attrs(self, obj):
+        # Iterate through the interesting attributes of the object:
+        for name in dir(obj):
+            # Ignore private and "magic" attributes:
+            if name.startswith('_'):
+                continue
+            value = getattr(obj, name)
+            # Ignore methods:
+            if hasattr(value, '__call__'):
+                continue
+            #print 'attr %r    obj.%s: %r' % (name, name, value)
+            yield (name, value)
+
+    def tree_to_dot(self, obj):
+        assert isinstance(obj, gcc.Tree)
+        result = ('  %s [label=<%s>];\n'
+                  % ('foo', self.label_for_tree(obj)))
+        for name, value in self.iter_attrs(obj):
+            print 'attr %r    obj.%s: %r' % (name, name, value)
+        return result
+
+    def to_dot(self):
+        self.root.debug()
+        result = 'digraph G {\n'
+        result += '  node [shape=record];\n'
+        result += self.tree_to_dot(self.root)
+        result += '}\n'
+        return result
+
 def cfg_to_dot(cfg):
     pp = CfgPrettyPrinter(cfg)
     return pp.to_dot()
 
+
+def tree_to_dot(tree):
+    pp = TreePrettyPrinter(tree)
+    return pp.to_dot()
