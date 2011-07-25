53f6134dc4c731e686f2cb24b717be14a0ff6266
David Malcolm <dmalcolm@redhat.com>
2011-07-25 15:13:34 -0400

    Python 3 fixes

--- show-ssa.py
+++ show-ssa.py
@@ -27,17 +27,17 @@ def my_pass_execution_callback(*args, **kwargs):
     if not optpass.properties_required & (1<<5):
         return
     if fun:
-        print 'fun.cfg: %r' % fun.cfg
+        print('fun.cfg: %r' % fun.cfg)
         if fun.cfg:
             #print help(fun.cfg)
-            print 'fun.cfg.basic_blocks: %r' % fun.cfg.basic_blocks
-            print 'fun.cfg.entry: %r' % fun.cfg.entry
-            print 'fun.cfg.exit: %r' % fun.cfg.exit
-            print 'fun.cfg.entry.succs: %r' % fun.cfg.entry.succs
-            print 'fun.cfg.exit.preds: %r' % fun.cfg.exit.preds
+            print('fun.cfg.basic_blocks: %r' % fun.cfg.basic_blocks)
+            print('fun.cfg.entry: %r' % fun.cfg.entry)
+            print('fun.cfg.exit: %r' % fun.cfg.exit)
+            print('fun.cfg.entry.succs: %r' % fun.cfg.entry.succs)
+            print('fun.cfg.exit.preds: %r' % fun.cfg.exit.preds)
             
             dot = cfg_to_dot(fun.cfg, fun.decl.name)
-            print dot
+            # print(dot)
             invoke_dot(dot)
 
 gcc.register_callback(gcc.PLUGIN_PASS_EXECUTION,
