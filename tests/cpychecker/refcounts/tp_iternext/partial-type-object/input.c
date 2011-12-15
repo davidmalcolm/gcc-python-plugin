davidmalcolm/gcc-python-plugin

commit e75656f113c7baf7a1d9b15f9c5f3455c6fcd93d

cpychecker: tp_iternext callbacks are allowed to return NULL without setting an exception
Detect functions that are used as a PyTypeObject's tp_iternext callback, and
suppress the "returns NULL without setting an exception" warning for them,
as it's legitimate for these callbacks to behave this way:

   http://docs.python.org/c-api/typeobj.html#tp_iternext

Fixes a false positive seen in rpm-4.9.1.2
   python/rpmts-py.c:rpmts_iternext
which is the rpmts_Type.tp_iternext callback

libcpychecker/initializers.py
@@ -167,4 +167,23 @@ def get_all_PyMethodDef_initializers():
                         result += table
     return result
 
+class PyTypeObjectInitializer(StructInitializer):
+    pass
 
+def get_all_PyTypeObject_initializers():
+    """
+    Locate all initializers for PyTypeObject, returning a list
+    of PyTypeObjectInitializer instances
+    """
+    log('get_all_PyTypeObject_initializers')
+
+    result = []
+    vars = gcc.get_variables()
+    for var in vars:
+        if isinstance(var.decl, gcc.VarDecl):
+            if str(var.decl.type) == 'struct PyTypeObject':
+                ctor = var.decl.initial
+                if ctor:
+                    si = PyTypeObjectInitializer(ctor)
+                    result.append(si)
+    return result

tests/cpychecker/refcounts/tp_iternext/input.c
@@ -1,0 +1,44 @@
+/*
+   Copyright 2011 David Malcolm <dmalcolm@redhat.com>
+   Copyright 2011 Red Hat, Inc.
+
+   This is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+*/
+
+#include <Python.h>
+
+/*
+  tp_iternext callbacks are allowed to returns NULL without setting an
+  exception.
+
+  Verify that the checker doesn't warn about such a case
+*/
+PyObject *
+test(PyObject *iter)
+{
+    return NULL;
+}
+
+static PyTypeObject test_type = {
+    .tp_iternext = test,
+};
+
+/*
+  PEP-7
+Local variables:
+c-basic-offset: 4
+indent-tabs-mode: nil
+End:
+*/

tests/cpychecker/refcounts/tp_iternext/script.py
@@ -1,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#   Copyright 2011 David Malcolm <dmalcolm@redhat.com>
+#   Copyright 2011 Red Hat, Inc.
+#
+#   This is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import gcc
+from libcpychecker import main
+
+main(verify_refcounting=True,
+     show_traces=False,
+     dump_traces=True)

libcpychecker/refcounts.py
@@ -2531,6 +2531,18 @@ class ExceptionStateAnnotator(Annotator):
 
         return result
 
+from libcpychecker.initializers import get_all_PyTypeObject_initializers
+def function_is_tp_iternext_callback(fun):
+    """
+    Is the given gcc.Function known to be used as the tp_iternext callback
+    within a PyTypeObject?
+    """
+    check_isinstance(fun, gcc.Function)
+    for typeobj in get_all_PyTypeObject_initializers():
+        tp_iternext = typeobj.function_ptr_field('tp_iternext')
+        if fun.decl == tp_iternext:
+            return True
+
 def check_refcounts(fun, dump_traces=False, show_traces=False,
                     show_possible_null_derefs=False,
                     show_timings=False):
@@ -2787,6 +2799,14 @@ def check_refcounts(fun, dump_traces=False, show_traces=False,
                     if (isinstance(endstate.cpython.exception_rvalue,
                                   ConcreteValue)
                         and endstate.cpython.exception_rvalue.value == 0):
+
+                        # Don't emit the error for functions that are a
+                        # PyTypeObject's tp_iternext callback, as it's
+                        # legitimate to return NULL from them:
+                        # http://docs.python.org/c-api/typeobj.html#tp_iternext
+                        if function_is_tp_iternext_callback(fun):
+                            return
+
                         err = rep.make_error(fun,
                                              endstate.get_gcc_loc(fun),
                                              'returning (PyObject*)NULL without setting an exception')

commit 1d4241d069b649d555740bf756e9c10170b9d354

cpychecker: fix traceback in tp_iternext handling
e75656f113c7baf7a1d9b15f9c5f3455c6fcd93d introduced a bug in the handling
of traces returning NULL without setting an exception.  The logic for
detecting tp_iternext callbacks falsely assumed that every PyTypeObject
initializer would initialize the tp_iternext field, leading to tracebacks
where this is implicitly initialized to NULL

Fix StructInitializer's *_field methods to return the implicit default
values.

Fixes tracebacks seen running the checker on PIL

libcpychecker/initializers.py
@@ -48,11 +48,17 @@ class StructInitializer(object):
         """
         Extract the initializer for the given field, as an int
         """
+        if fieldname not in self.fielddict:
+            return 0 # implicit 0
+
         tree = self.fielddict[fieldname]
         check_isinstance(tree, gcc.IntegerCst)
         return tree.constant
 
     def char_ptr_field(self, fieldname):
+        if fieldname not in self.fielddict:
+            return None # implicit NULL
+
         tree = self.fielddict[fieldname]
         if isinstance(tree, gcc.IntegerCst):
             if tree.constant == 0:
@@ -69,6 +75,9 @@ class StructInitializer(object):
         Extract the initializer for the given field, as a gcc.FunctionDecl,
         or None for NULL.
         """
+        if fieldname not in self.fielddict:
+            return None # implicit NULL
+
         tree = self.fielddict[fieldname]
 
         # go past casts:

tests/cpychecker/refcounts/tp_iternext/partial-type-object/input.c
@@ -1,0 +1,49 @@
+/*
+   Copyright 2011 David Malcolm <dmalcolm@redhat.com>
+   Copyright 2011 Red Hat, Inc.
+
+   This is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+*/
+
+#include <Python.h>
+
+/*
+  tp_iternext callbacks are allowed to returns NULL without setting an
+  exception.
+
+  Verify that the logic for this doesn't croak on a PyTypeObject that
+  doesn't have an initializer for tp_iternext
+*/
+PyObject *
+test(PyObject *obj)
+{
+    /* The checker ought to report on the lack of an exception: */
+    return NULL;
+}
+
+static PyTypeObject test_type = {
+    /*
+      empty initializer; in particular, lacking an initializer
+      for tp_iternext
+    */
+};
+
+/*
+  PEP-7
+Local variables:
+c-basic-offset: 4
+indent-tabs-mode: nil
+End:
+*/

tests/cpychecker/refcounts/tp_iternext/partial-type-object/script.py
@@ -1,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#   Copyright 2011 David Malcolm <dmalcolm@redhat.com>
+#   Copyright 2011 Red Hat, Inc.
+#
+#   This is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import gcc
+from libcpychecker import main
+
+main(verify_refcounting=True,
+     show_traces=False,
+     dump_traces=True)