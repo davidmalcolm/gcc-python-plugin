1c9dd1af41629140fd54f13142342b438fc56f74
David Malcolm <dmalcolm@redhat.com>
2012-02-07 12:31:38 -0500

    mass-rebuild.py: remove hardcoded list of SRPMs; reenable --init

--- misc/fedora/mass-rebuild.py
+++ misc/fedora/mass-rebuild.py
@@ -15,6 +15,7 @@
 #   along with this program.  If not, see
 #   <http://www.gnu.org/licenses/>.
 
+import glob
 import os
 import re
 import shutil
@@ -64,7 +65,7 @@ for srpmname in get_local_python_srpms():
 def local_rebuild_of_srpm_in_mock(srpmpath, mockcfg):
     """
     Rebuild the given SRPM locally within mock, injecting the cpychecker
-    code in RPM form
+    code in RPM form; gathering the results to a subdir within "LOGS"
     """
     def run_mock(commands, captureOut=False, captureErr=False):
         cmds = ['mock', '-r', mockcfg] + commands
@@ -81,12 +82,13 @@ def local_rebuild_of_srpm_in_mock(srpmpath, mockcfg):
         return out, err
 
     n, v, r = nvr_from_srpm_path(srpmpath)
-    resultdir = '%s-%s-%s' % (n, v, r)
+    resultdir = 'LOGS/%s-%s-%s' % (n, v, r)
     if os.path.exists(resultdir):
         shutil.rmtree(resultdir)
     os.mkdir(resultdir)
         
-    if 0:
+    # Experimenting with the script is much faster if we remove the --init here:
+    if 1:
         run_mock(['--init'])
     run_mock(['--installdeps', srpmpath])
     run_mock(['install', PLUGIN_PATH]) # this doesn't work when cleaned: can't open state.log
@@ -118,8 +120,7 @@ def local_rebuild_of_srpm_in_mock(srpmpath, mockcfg):
 PLUGIN_PATH='gcc-python2-plugin-0.9-1.fc16.x86_64.rpm'
 MOCK_CONFIG='fedora-16-x86_64'
 
-# For now, just an experimental hardcoded list of srpms:
-local_rebuild_of_srpm_in_mock('MySQL-python-1.2.3-3.fc16.src.rpm')
-local_rebuild_of_srpm_in_mock('python-crypto-2.3-5.fc16.src.rpm')
-local_rebuild_of_srpm_in_mock('rpm-4.9.1.2-1.fc16.src.rpm')
+# Rebuild all src.rpm files found in "SRPMS":
+for srpmpath in glob.glob('SRPMS/*.src.rpm'):
+    local_rebuild_of_srpm_in_mock(srpmpath, MOCK_CONFIG)
 
