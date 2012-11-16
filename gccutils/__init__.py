bd3861606a787a0cabab9a3d1249acc4c2d775e2
David Malcolm <dmalcolm@redhat.com>
2012-11-16 15:50:04 -0500

    don't import six in gccutils.__init__.py

--- gccutils/__init__.py
+++ gccutils/__init__.py
@@ -19,8 +19,6 @@ import sys
 
 import gcc
 
-from six.moves import xrange
-
 def sorted_dict_repr(d):
     return '{' + ', '.join(['%r: %r' % (k, d[k])
                             for k in sorted(d.keys())]) + '}'
@@ -565,7 +563,7 @@ class Table(object):
 
     def _calc_col_widths(self):
         result = []
-        for colIndex in xrange(self.numcolumns):
+        for colIndex in range(self.numcolumns):
             result.append(self._calc_col_width(colIndex))
         return result
 
