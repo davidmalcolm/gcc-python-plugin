a102d86358b33cdcfd0dea89615947ceea20c072
Plamen <plamen.petkov@broadcom.com>
2014-09-26 11:46:11 +0100

    Ensure size is non-zero.

--- gcc-python-tree.c
+++ gcc-python-tree.c
@@ -542,7 +542,8 @@ PyGccFunction_TypeObj_get_argument_types(struct PyGccTree * self, void *closure)
 {
     PyObject *result;
     PyObject *item;
-    int i, size;
+    int i;
+    unsigned int size;
     tree iter;
     tree head = TYPE_ARG_TYPES(self->t.inner);
 
@@ -554,10 +555,14 @@ PyGccFunction_TypeObj_get_argument_types(struct PyGccTree * self, void *closure)
     }
 
     /* "size" should now be the length of the chain */
+    
+    /* When a function with no input arguments is passed ( ex int foo())
+       the previous code crashed. */
+    if(size == 0)
+        size = 1;
 
     /* The last element in the list is a VOID_TYPE; don't add this;
        see dump_function_declaration() in gcc/tree-pretty-print.c */
-    assert(size>0);
     size--;
 
     result = PyTuple_New(size);
